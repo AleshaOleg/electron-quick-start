69ad26e385f960e924b089ca66dcf1256a4fd828
Oleh Aloshkin <olegaleshkin@gmail.com>
2020-06-10 17:11:55 +0200

    Added example of promisify-electron-ipc usage

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+const { promiseIpcMain } = require('promisify-electron-ipc')
 const path = require('path')
 
 function createWindow () {
@@ -8,7 +9,8 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
     }
   })
 
@@ -41,3 +43,6 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+promiseIpcMain.on("greet-channel", name => {
+  return Promise.resolve(`Hello ${name}`)
+})
--- renderer.js
+++ renderer.js
@@ -4,3 +4,13 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const { promiseIpcRenderer } = require('promisify-electron-ipc')
+
+document.querySelectorAll('.button').forEach(button => {
+  button.addEventListener('click', () => {
+    promiseIpcRenderer
+      .send("greet-channel", button.textContent)
+      .then(answer => console.log(answer)) // prints `Hello ${name}`
+  })
+})
